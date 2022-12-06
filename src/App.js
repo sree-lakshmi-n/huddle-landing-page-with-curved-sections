c32ceee69a7a4be3e958f2bd37de29ffcead3124
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-06 16:31:49 +0530

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="container">
       <Nav />
+      <Header />
     </div>
   );
 }
