5a99f680b9c7b0e0a57d1d02720fa9c4dd9d7105
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-07 06:49:50 +0530

    added Stats component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
+import Stats from "./components/Stats/Stats";
 
 function App() {
   return (
     <div className="container">
       <Nav />
       <Header />
+      <Stats />
     </div>
   );
 }
