e9b8c2e8e61ecb0367578245385ecc1a52366808
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-07 13:54:59 +0530

    enclosed feature-card in feature-card-wrapper

--- src/components/Featues/FeatureCard.js
+++ src/components/Featues/FeatureCard.js
@@ -1,13 +1,15 @@
 import "./FeatureCard.css";
 const FeatureCard = (props) => {
   return (
-    <div className="feature-card">
-      <div className="feature-card__imgbox">
-        <img src={props.features.image} alt={props.features.altMsg} />
-      </div>
-      <div className="feature-card__contentbox">
-        <p className="feature-card__title">{props.features.title}</p>
-        <p className="feature-card__content">{props.features.content}</p>
+    <div className="feature-card-wrapper">
+      <div className="feature-card">
+        <div className="feature-card__imgbox">
+          <img src={props.features.image} alt={props.features.altMsg} />
+        </div>
+        <div className="feature-card__contentbox">
+          <p className="feature-card__title">{props.features.title}</p>
+          <p className="feature-card__content">{props.features.content}</p>
+        </div>
       </div>
     </div>
   );
