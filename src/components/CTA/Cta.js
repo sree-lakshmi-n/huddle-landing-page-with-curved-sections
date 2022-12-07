493e299f2bf2e32861c8a04c90e7c6c33eee1bd8
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-07 17:12:07 +0530

    cta component and its contents added

---
+++ src/components/CTA/Cta.js
@@ -0,0 +1,10 @@
+import "./Cta.css";
+const Cta = () => {
+  return (
+    <div className="cta-section">
+      <p className="title">Ready To Build Your Community?</p>
+      <button className="btn btn-cta"> Get Started For Free</button>
+    </div>
+  );
+};
+export default Cta;
