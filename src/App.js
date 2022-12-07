1e7a7dd013a7dcb6c2cedd1b851b59a979f0299a
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-07 09:05:49 +0530

    added features component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
 import Stats from "./components/Stats/Stats";
-
+import Features from "./components/Featues/Features";
 function App() {
   return (
     <div className="container">
       <Nav />
       <Header />
       <Stats />
+      <Features />
     </div>
   );
 }
