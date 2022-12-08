da382922639574601c04b0f16b88a1b5c2fc6b54
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-08 17:59:11 +0530

    Contact component added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import "./Footer.css";
 import Newsletter from "./Newsletter";
+import Contact from "./Contact";
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-contents">
         <Newsletter />
+        <Contact />
       </div>
     </div>
   );
