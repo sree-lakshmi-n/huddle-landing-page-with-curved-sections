7274af9a8d49fb249aaec28048f548d34fe662d3
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-08 11:21:30 +0530

    Newsletter component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
 import "./Footer.css";
+import Newsletter from "./Newsletter";
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <Newsletter />
+    </div>
+  );
 };
 export default Footer;
